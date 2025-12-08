852c3c8c12ff095ec9a3acae044ab75f3c17ee5a
jayashreev <jayashreev@niya.app>
2025-12-08 16:37:35 +0530

    Fix: Filter blank values in ActiveAdmin controller for coach specializations

--- back-end/app/admin/coach_specializations.rb
+++ back-end/app/admin/coach_specializations.rb
@@ -2,6 +2,24 @@ ActiveAdmin.register CoachSpecialization do
   menu label: "Coach Specialization"
   permit_params :expertise, :focus_areas => []
   filter :expertise
+
+  controller do
+    def create
+      # Clean up focus_areas before creating the record
+      if params[:coach_specialization][:focus_areas].is_a?(Array)
+        params[:coach_specialization][:focus_areas] = params[:coach_specialization][:focus_areas].reject(&:blank?)
+      end
+      super
+    end
+
+    def update
+      # Clean up focus_areas before updating the record
+      if params[:coach_specialization][:focus_areas].is_a?(Array)
+        params[:coach_specialization][:focus_areas] = params[:coach_specialization][:focus_areas].reject(&:blank?)
+      end
+      super
+    end
+  end
   index :download_links => false do 
       selectable_column
       id_column
